bc4de5fb9669e8c5b4cc5f986f87979561ccf1ca
Deanna Swallow <swallowdeanna@gmail.com>
2019-06-27 20:34:03 -0600

    Add pictures to choices for character

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   state = {
     characters: characters,
     items: items,
-    selectedOption: "option1"
+    selectedOption: "Mario"
   };
 
   handleOptionChange = changeEvent => {
@@ -23,109 +23,45 @@ class App extends React.Component {
         <h1>What Your Mario Kart Play Style Says About You!</h1>
         <Sprite sprite={"mario"} />
         <p>
-          As was discussed here, the character you choose when playing Mario
-          Kart says a lot about who you are at heart. I would argue that your
-          favorite item also says a lot. So what does your favorite character
-          and item in Mario Kart say about you? Answer the questions below to
-          find out!
+          As was discussed{" "}
+          <a
+            href="https://twitter.com/chloecondon/status/1108738908999700480"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here,
+          </a>
+          the character you choose when playing Mario Kart says a lot about who
+          you are at heart. I would argue that your favorite item also says a
+          lot (I'm looking at you, banana peel throwers!). So what does your
+          favorite character and item in Mario Kart say about you? Answer the
+          questions below to find out!
         </p>
         <h3>1. Which character is your favorite to use?</h3>
         <Container rounded>
           <form className="character-choice">
-            <label>
-              <input
-                type="radio"
-                name="characters"
-                value="option1"
-                checked={this.state.selectedOption === "option1"}
-                onChange={this.handleOptionChange}
-                className="character-choice"
-              />
-              Mario
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="characters"
-                value="option2"
-                checked={this.state.selectedOption === "option2"}
-                onChange={this.handleOptionChange}
-                className="character-choice"
-              />
-              Luigi
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="characters"
-                value="option3"
-                checked={this.state.selectedOption === "option3"}
-                onChange={this.handleOptionChange}
-                className="character-choice"
-              />
-              Princess Peach
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="characters"
-                value="option4"
-                checked={this.state.selectedOption === "option4"}
-                onChange={this.handleOptionChange}
-                className="character-choice"
-              />
-              Toad
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="characters"
-                value="option5"
-                checked={this.state.selectedOption === "option5"}
-                onChange={this.handleOptionChange}
-                className="character-choice"
-              />
-              Yoshi
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="characters"
-                value="option6"
-                checked={this.state.selectedOption === "option6"}
-                onChange={this.handleOptionChange}
-                className="character-choice"
-              />
-              Bowser
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="characters"
-                value="option7"
-                checked={this.state.selectedOption === "option7"}
-                onChange={this.handleOptionChange}
-                className="character-choice"
-              />
-              Donkey Kong Jr.
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="characters"
-                value="option8"
-                checked={this.state.selectedOption === "option8"}
-                onChange={this.handleOptionChange}
-                className="character-choice"
-              />
-              Koopa Troopa
-            </label>
+            {Object.values(characters).map(character => (
+              <label key={character.name}>
+                <input
+                  type="radio"
+                  name="characters"
+                  value={character.name}
+                  // checked={false}
+                  onChange={this.handleOptionChange}
+                  className="character-choice"
+                />
+                {character.name}
+                <img src={character.image} alt={character.name} />
+              </label>
+            ))}
           </form>
         </Container>
         <h3>2. Which item is your favorite to use?</h3>
         <Container rounded>Item info here</Container>
         <h2>What your choices say about you as a person:</h2>
-        <Container rounded>Profile here</Container>
+        <Container rounded>
+          {this.state.selectedOption === "Yoshi" ? "Yay" : "Nope"}
+        </Container>
       </div>
     );
   }
